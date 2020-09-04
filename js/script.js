9ceae0061db427c84acbcc4d0f28b299f2d198cd
AngiAustin <66363889+AngiAustin@users.noreply.github.com>
2020-09-04 09:46:42 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,6 +34,7 @@ WARNING!!! WARNING!!!
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  } () );
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
